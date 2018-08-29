43a2c25e27c0d97ce82807acccfbbf62bb7896b7
mjturt <maks.turtiainen@protonmail.com>
2018-08-29 18:06:05 +0300

    lesskey envar

--- .zsh/evars.zsh
+++ .zsh/evars.zsh
@@ -9,5 +9,6 @@ export RANGER_LOAD_DEFAULT_RC=TRUE
 export PAGER="less"
 export LESS="-R"
 export LESSOPEN="|~/.lessfilter %s"
+export LESSKEY="$HOME/.less"
 LS_COLORS='rs=0:di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:su=37;41:sg=30;43:tw=30;42:ow=34;42:st=37;44:ex=01;32:';
 export LS_COLORS
